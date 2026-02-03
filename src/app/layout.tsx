84e217cb3dc7019803a77aef80183efd6adea0ce
Finn <finn.nkj@Finn-MacBook-Pro.local>
2026-02-03 12:36:15 +0900

    Change favicon to calendar emoji

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "약속 날짜 정하기",
   description: "친구들과 함께 가능한 날짜를 선택하세요",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📅</text></svg>",
+  },
 };
 
 export default function RootLayout({
